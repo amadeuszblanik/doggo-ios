223f706ccb38c4952568cf796fb4fb53567eb4aa
Amadeusz Blanik <amadeusz@blanik.me>
2022-06-30 01:36:04 +0200

    Initial Commit

---
+++ Shared/DoggoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoggoApp.swift
+//  Shared
+//
+//  Created by Amadeusz Blanik on 30/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct DoggoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
